a3129882833f0ce943e9c0cb226c95b9e8c31013
Alvaro Rodrigues <adanielssr@gmail.com>
2017-08-20 14:10:44 +0100

    Implement Integration tests (still tests missing)

--- api/api-boot/src/test/java/com/github/adanielssr/merchant/offers/api/boot/OffersControllerIT.java
+++ api/api-boot/src/test/java/com/github/adanielssr/merchant/offers/api/boot/OffersControllerIT.java
@@ -2,25 +2,31 @@ package com.github.adanielssr.merchant.offers.api.boot;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
 import com.github.adanielssr.merchant.offers.api.service.ServiceConfiguration;
 import com.github.adanielssr.merchant.offers.business.config.BusinessConfiguration;
 import com.github.adanielssr.merchant.offers.domain.DomainConfiguration;
+import com.github.adanielssr.merchant.offers.dto.OfferDto;
 import com.github.springtestdbunit.DbUnitTestExecutionListener;
 import com.github.springtestdbunit.annotation.DatabaseSetup;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+
+import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = { Application.class, ServiceConfiguration.class, BusinessConfiguration.class,
@@ -32,29 +38,72 @@ public class OffersControllerIT {
 
     public static final String OFFERS_PATH = "/offers";
 
+    public static final String MESSAGE_SPRING_KEY = "message";
+
+    private static final Long GOOD_ID = 1L;
+
+    private static final Long MERCHANT_ID = 1L;
+
     @Autowired
     private TestRestTemplate restTemplate;
 
-    @Test
-    @DatabaseSetup("/db/FilledRepository.xml")
-    @DirtiesContext
-    public void testFindAllTags() throws IOException {
-        List allTags = this.restTemplate.getForObject(OFFERS_PATH, List.class);
-
-        Assert.assertNotNull(allTags);
-
-        Assert.assertEquals(3, allTags.size());
-    }
+    private Gson gson = new GsonBuilder().create();
 
     @Test
     @DatabaseSetup("/db/EmptyRepository.xml")
-    @DirtiesContext
-    public void testFindAllTags_Empty() throws IOException {
-        List allTags = this.restTemplate.getForObject(OFFERS_PATH, List.class);
+    public void testFindAllOffers_Empty() throws IOException {
+        List allOffers = this.restTemplate.getForObject(OFFERS_PATH, List.class);
 
-        Assert.assertNotNull(allTags);
+        assertNotNull(allOffers);
 
-        Assert.assertEquals(0, allTags.size());
+        assertEquals(0, allOffers.size());
+    }
+
+    @Test
+    @DatabaseSetup("/db/FilledRepository.xml")
+    public void testFindAllOffers() throws IOException {
+        List allOffers = this.restTemplate.getForObject(OFFERS_PATH, List.class);
+
+        assertNotNull(allOffers);
+
+        assertEquals(2, allOffers.size());
+    }
+
+    @Test
+    @DatabaseSetup("/db/EmptyRepositoryWithProductAndGood.xml")
+    public void testCreateOfferMissingCurrencyCode() throws IOException {
+        OfferDto offerDto = buildNewOfferDto();
+        offerDto.setCurrencyCode(null);
+
+        ResponseEntity<String> createdOfferDtoResponse = this.restTemplate
+                .postForEntity(OFFERS_PATH, offerDto, String.class);
+
+        assertEquals(HttpStatus.BAD_REQUEST, createdOfferDtoResponse.getStatusCode());
+        Map<String, Object> jsonError = gson.fromJson(createdOfferDtoResponse.getBody(), Map.class);
+
+        assertTrue(jsonError.containsKey(MESSAGE_SPRING_KEY));
+        assertTrue(((String) jsonError.get(MESSAGE_SPRING_KEY)).contains("currencyCode"));
+    }
+
+    @Test
+    @DatabaseSetup("/db/EmptyRepositoryWithProductAndGood.xml")
+    public void testCreateOfferMissingNewPrice() throws IOException {
+        OfferDto offerDto = buildNewOfferDto();
+        offerDto.setNewPrice(null);
+
+        ResponseEntity<String> createdOfferDtoResponse = this.restTemplate
+                .postForEntity(OFFERS_PATH, offerDto, String.class);
+
+        assertEquals(HttpStatus.BAD_REQUEST, createdOfferDtoResponse.getStatusCode());
+        Map<String, Object> jsonError = gson.fromJson(createdOfferDtoResponse.getBody(), Map.class);
+
+        assertTrue(jsonError.containsKey(MESSAGE_SPRING_KEY));
+        assertTrue(((String) jsonError.get(MESSAGE_SPRING_KEY)).contains("newPrice"));
+    }
+
+    private OfferDto buildNewOfferDto() {
+        return OfferDto.builder().currencyCode("EUR").newPrice(10.0D).description("Offer for Product A")
+                .relatedGoodId(GOOD_ID).merchantOwnerId(MERCHANT_ID).build();
     }
 
 }
